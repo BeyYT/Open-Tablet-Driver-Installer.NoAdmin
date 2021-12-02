4bffbaf461d7b53333a93126f7a010bab10ec078
BeyYT <60990459+BeyYT@users.noreply.github.com>
2021-12-02 10:10:44 -0700

    added copyright stuff and added path addition
    dsadasdsadsadsasdsadsadsa

--- src/installer.py
+++ src/installer.py
@@ -1,14 +1,12 @@
 # Import Modules
 
-import urllib.request
-import zipfile
-import os
-import winshell
+import urllib.request, zipfile, os, winshell
 from win32com.client import Dispatch
 
 # Say hi
 
 print("Hello! Welcome To The Tablet Driver Installer.")
+print("Copyright BeyYT 2021. All Rights Reserved, OpenTabletDriver Is A Part Of The OpenTabletDriver Group.")
 input("Press Enter To Continue")
 print("Downloading Driver")
 
@@ -19,7 +17,7 @@ urllib.request.urlretrieve("https://github.com/OpenTabletDriver/OpenTabletDriver
 
 print("Downloading .NET 5.0 Framework")
 
-# If Your Distro Has NET 5.0 Framework Embedded, Comment Line 18, 22, 30-32, 36-39 and 45.
+# If Your Distro Has NET 5.0 Framework Embedded, Comment Line 22, 30-33, 37-42 and 46.
 
 urllib.request.urlretrieve("https://download.visualstudio.microsoft.com/download/pr/d1ca6dbf-d054-46ba-86d1-36eb2e455ba2/e950d4503116142d9c2129ed65084a15/dotnet-sdk-5.0.403-win-x64.zip", "dotnet-sdk-5.0.403-win-x64.zip")
 
@@ -34,10 +32,13 @@ with zipfile.ZipFile(os.getcwd() + "\\dotnet-sdk-5.0.403-win-x64.zip", 'r') as z
     zip_ref.extractall(os.getcwd() + "\\.Net_5.0_Framework")
 
 # Path Entries (If .NET 5.0 Framework Is Not Embedded)
-
+# Usually Requires Local PATH To Be Enabled By SYSADMIN
 print("adding PATH entries")
+
 os.system('setx DOTNET_ROOT "' + os.getcwd() + '\\.Net_5.0_Framework"')
-os.system('setx PATH "' + os.getcwd() + '\\.Net_5.0_Framework"')
+#Get Current Path Entries
+curpath = os.getenv(['PATH'])
+os.system('setx PATH "' + curpath + ';' + os.getcwd() + '\\.Net_5.0_Framework"')
 os.system('setx DOTNET_MULTILEVEL_LOOKUP 0')
 
 # Printing stuff
@@ -45,6 +46,9 @@ os.system('setx DOTNET_MULTILEVEL_LOOKUP 0')
 print("done!")
 print("Tablet Driver Path:" + os.getcwd() + "\\Driver")
 print(".Net 5.0 Framework Path:" + os.getcwd() + "\\.Net_5.0_Framework")
+
+# deleting Useless ZIP Files
+
 os.remove(os.getcwd() + "\\dotnet-sdk-5.0.403-win-x64.zip")
 os.remove(os.getcwd() + "\\OpenTabletDriver.win-x64.zip")
 
@@ -62,3 +66,8 @@ shortcut.WorkingDirectory = wDir
 shortcut.IconLocation = icon
 shortcut.save()
 input("Press the Enter key to exit.")
+
+# Hey!
+# In Case It Stops Working Or Dosent Work, Please Send Me A DM on Discord
+# Bey#0513
+
